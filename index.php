6c8d821737dbedf223c3fe1ff9235047ee730689
EndoHaxor404 <44937661+EndoHaxor404@users.noreply.github.com>
2018-11-11 16:53:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<html>
+<head>
+<link rel="shortcut icon" href="https://1.bp.blogspot.com/-t5REKpFtyRE/WIAFMWutPUI/AAAAAAAAB9Y/T7M7PD0NBAkL7g0vgOVtRKXHyp5MhOP7ACEw/s1600/vektor%2Bgaruda.jpg">
+<link href='https://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet" type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<title> Pwnd By ./E4OXY </title>
+	<body bgcolor=black>
+		<center>
+			
+			<font color=white face=Ubuntu size=5>
+				<br><br><br><b>
+				 Stamped By Garuda Tersakti 72</b> </b> <br>
+				 -[ </font><font color=red face=Ubuntu size=5>./E4OXY</font><font color=white face=Ubuntu size=5> ]-</font>
+				 <br><br>
+				 <img src='https://scontent-sea1-1.cdninstagram.com/vp/38a073151c868f2f7347cfeb88a97315/5C08045B/t51.2885-15/sh0.08/e35/s750x750/32824385_160046161506766_6911471753659154432_n.jpg' height=350 width=350>
+				 <br><br><br><font size=6 face=Iceland color=white>#IndonesianDefacers<br> </font><br> 
+				 <font color=white face=Iceland size=5>Member Garuda Tersakti 72 :</font>
+				 	<br><font color=red face=Iceland size=4>[ ./E4OXY ]</font><font color=white face=Iceland size=4>[ ./E4OXY ] - [ TN72 ] - [ ./GH05TW1BL3 ] - [ DarkOct02 ] <br> [ M1NT_1X ] - [ Cy#b3r0ne ] - [ Doraemon v1.5 ] - [ ./CE.19 ]<br> [ ./Kazu_ID ] - [ GrimX72 ] - [ BravOne ] - [ Yukki666 ] - [ Bl0ds_touch ] - [ XT72 ] - [ SatanicHeartz ]
+				 	 <br><br></font><font color=white face=Iceland size=5> My Friends :</font>
+				 	 <br> <font color=white face=Iceland size=4>TehSosro404 - HT45TP - KID2ZON3 - ./sT0ry_mB3m - Mr.L3gacy - JAN9KR1K - D404Zr - ./Ikari404 - Mr.Joker366 -  and You </font>
+				 	 <br><br><br><font color=white face=Iceland size=5>My Contact [ indoxeze.id@gmail.com ] <br> Website  [ http://garudatersakti72.id ]</font>
+
+
